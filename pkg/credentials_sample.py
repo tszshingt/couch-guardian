01530ee68c4c018fce2f8151d2b5c76526039cd5
Shing <tszshingt@yahoo.com.hk>
2019-11-24 04:36:42 -0500

    sample credential file

---
+++ pkg/credentials_sample.py
@@ -0,0 +1,10 @@
+# IBM Cloudant IAM credentials:
+deviceID = "deviceID"
+username = "username"
+apikey = "apikey"
+
+# IBM WatsonIoT credentials:
+orgId = "orgId"
+typeId = "typeId"
+deviceId = "deviceId"
+token = "token"
